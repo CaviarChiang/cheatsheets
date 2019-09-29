d4742e3007876397dadb9427cc2ac192f0ead06e
CaviarChiang <yuyanj143@foxmail.com>
2019-09-29 02:49:02 -0400

    Added my kyle cmp

--- react/myapp/src/index.js
+++ react/myapp/src/index.js
@@ -6,6 +6,7 @@ class KyleBaby extends Component {
 
   constructor() {
     super();
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       greeting: 'default',
     };
@@ -17,10 +18,42 @@ class KyleBaby extends Component {
     });
   }
 
+  handleClick(event) {
+    this.setState({
+      greeting: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>Hi Caviar, {this.state.greeting}</h2>
+        <Button handleClick={this.handleClick} />
+      </div>
+    )
+  }
+}
+
+class Button extends Component {
+  constructor() {
+    super();
+    this.state = {
+      value: "",
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.handleChange}></input>
+        <button onClick={this.props.handleClick} value={this.state.value}>Suck my tator</button>
       </div>
     )
   }
@@ -41,7 +74,7 @@ class App extends Component {
       <div className="App">
         <KyleBaby greeting="I, Kyle, miss you" />
         {this.sayHi("kyle")}
-        <h3>Hi Kyle!</h3>
+        <KyleBaby greeting="You are the best baby" />
       </div>
     )
   }
